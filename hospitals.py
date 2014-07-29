ba7628677e3242bb6b1c171a62e06e47711b16a3
Matthew Hall <matthew@quickbeam.me.uk>
2014-07-29 09:40:18 +0100

    Dump places as JSON

--- hospitals.py
+++ hospitals.py
@@ -1,7 +1,13 @@
-from models import db, app
+import json
+from models import db, app, Place
 
-@app.route("/hi")
-def hi():
-    return 'hi'
+@app.route("/allplaces")
+def allplaces():
+    places = Place.query.all()
+    places = [{'id': place.id, 'name': place.name, 'tel': place.telephone, 
+               'street': place.street, 'locality': place.locality, 'region': place.region, 
+               'postcode': place.postcode, 'lat': place.lat, 'lng': place.lng} for place in places]
+    places_json = json.dumps(places)
+    return places_json
 
-app.run()
+app.run(debug=True)
