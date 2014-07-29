ab2c3781a96a27dcdb740b2cce606f1d72a3561c
Matthew Hall <matthew@quickbeam.me.uk>
2014-07-29 19:02:28 +0100

    Add comments to javascript files

--- static/js/bars.js
+++ static/js/bars.js
@@ -1,13 +1,20 @@
+// the width of a bar
 w = 100;
+// the height of a bar
 h = 20;
+// the multiplier to go from a rating to a width
 mult = 20;
 
-function create_bar(name, rating) {
+function create_bar(element_id, rating) {
+    // let's make sure that all ratings are positive. Should be ensured by the
+    // importer but better safe that sorry!
     if (rating > 0) {
-        var svg = d3.select("#"+name)
+        // create an svg at the element with element_id as it's id
+        var svg = d3.select("#"+element_id)
                     .append("svg")
                     .attr("width", w)
                     .attr("height", h);
+        // colour the whole bar red to start with
         svg.append("rect")
            .attr("x", 0)
            .attr("y", 0)
@@ -15,15 +22,17 @@ function create_bar(name, rating) {
            .attr("height", h)
            .attr("fill", "red");
 
+        // colour part of the bar green to show how good the rating is
+        // this will look sort of like a progress bar
         svg.append("rect")
            .attr("x", 0)
            .attr("y", 0)
            .attr("width", rating * mult)
            .attr("height", h)
            .attr("fill", "green");
-        $('<td class="rating">' + rating + '</td>').insertAfter("#"+name);
-    }
-    else {
+        // insert the actual rating so the data is available
+        $('<td class="rating">' + rating + '</td>').insertAfter("#"+element_id);
+    } else {
         $("#"+name).text("No rating found");
     }
 }
--- static/js/map_and_list.js
+++ static/js/map_and_list.js
@@ -1,4 +1,6 @@
 function load_list(places) {
+    // remove all data from the list. This is incase some of the places listed
+    // don't need to be included anymore
     var list = $('#hospitals-list').html('');
     $.each(places, function(i, place) {
         div = '<div class="hospital-list-item"><a href="/place/' + place.id + 
@@ -11,6 +13,8 @@ function load_list(places) {
 function load_map(json, map) {
     var markers = [];
     $.each(json, function(i, place) {
+        // for every place add a marker with an infowindow containing a link to
+        // it's page
         var info = new google.maps.InfoWindow({
             content: '<a href="/place/' + place.id + '">' + place.name + "</a>",
         });
@@ -23,7 +27,9 @@ function load_map(json, map) {
         });
         markers.push({m: marker, p: place});
     });
+    // when the user changes the zoom/position of the map
     google.maps.event.addListener(map, 'bounds_changed', function() {
+        // find all the visible markers and include only them on the list
         places = [];
         $.each(markers, function(i, marker) {
             if (map.getBounds().contains(marker.m.getPosition())) {
@@ -41,11 +47,16 @@ function load_data(map) {
     });
 }
 
+// entry point for all initialisers
 function initialise_map() {
+    // centre around sheffield
     var map_options = {center: new google.maps.LatLng(53.420882, -1.208496), zoom: 6};
     var map = new google.maps.Map(document.getElementById('map-canvas'), map_options);
+    // add the data to the map and to the list
     load_data(map);
 
+    // if the search button is clicked or the enter key is pressed in the
+    // search box, search for the place name in the search box
     $('#search-btn').click(function () {
         search(map);
     });
@@ -60,6 +71,7 @@ function initialise_map() {
 function search(map) {
     query = $('#search').val();
     $.getJSON('/placelocation', {q: query}, function(json) {
+        // go to the coordinates given
         var latlng = new google.maps.LatLng(json.lat, json.lng);
         map.panTo(latlng);
         map.setZoom(12);
